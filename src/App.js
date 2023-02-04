08fa292e0c39d6b5be9eb62f2fa110b792d7b39a
Zaw Khing <zawmyokhing@gmail.com>
2023-02-04 15:30:24 +0000

    Adding weather image

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
     <div className="App">
       <Header title={siteTitle} />
       <main className="c-site-main" tabIndex="0">
+        <img
+          className="weather-img"
+          src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"
+          alt="Current Weather"
+        />
+        {/* fetch the data
+        images
+        current Weather
+        future Weather */}
       </main>
       <Footer />
     </div>
