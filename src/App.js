29cfdbe4b2e5051e93ccdc15143dd39bfa673fe0
Lunamzelo <lunamzelo@gmail.com>
2023-02-04 15:01:01 +0000

    rendered the future weather component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import FutureWeather from "./components/FutureWeather/FutureWeather";
+
 
 //configs
 const siteTitle = process.env.REACT_APP_SITE_TITLE ?? "CYF Weather";
@@ -10,7 +12,9 @@ function App() {
     <div className="App">
       <Header title={siteTitle} />
       <main className="c-site-main" tabIndex="0">
+        <FutureWeather weatherArray={[1, 2, 3, 4, 5, 6, 7]} />
       </main>
+
       <Footer />
     </div>
   );
