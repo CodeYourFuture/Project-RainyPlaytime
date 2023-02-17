dac08f8d171379df3735a7dd8aafae3b91c02e08
Zaw Khing <zawmyokhing@gmail.com>
2023-02-17 11:20:25 +0000

    Fetching API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+// import FakeWeatherData from "./data/FakeWeather.json";
 
-//configs
+
 const siteTitle = process.env.REACT_APP_SITE_TITLE ?? "CYF Weather";
 
 function App() {
+
   return (
     <div className="App">
       <Header title={siteTitle} />
+
       <main className="c-site-main" tabIndex="0">
-        <img
-          className="weather-img"
-          src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"
-          alt="Current Weather"
-        />
-        {/* fetch the data
-        images
-        current Weather
-        future Weather */}
+        <div>
+          <img
+            className="weather-img"
+            src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"
+            alt="Current Weather"
+          />
+        </div>
+
+        
+
+        {/* {FakeWeatherData.list.map((data) => (
+          <div key={data.dt}>Temp : {data && data.main.temp}</div>
+        ))} */}
+
+        {/* <div>{location}</div> */}
+        {/* <div>{temp && temp.list[0].main.temp}</div> */}
       </main>
+
       <Footer />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-import './Header.scss'
+import "./Header.scss";
+import React, { useState, useEffect } from "react";
 
-const Header = ({title}) =>
+const Header = ({ title }) => {
+  const [data, setData] = useState([]);
+  // const [city, setCity] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=liverpool&units=metric&appid=3b86046cce0de3be7cfa8369f4540b37`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.main.temp.toFixed());
+        setData(data);
+      })
+      .catch((e) => console.log(e.message));
+  }, []);
+
+  return (
     <header className="c-site-header">
-        <h1 className="c-site-header__title">{title}</h1>
-    </header>
+      <h1 className="c-site-header__title">{title}</h1>
 
+      <input
+        className="city"
+        type="text"
+        // value={city}
+        // onChange={(event) => setCity(event.target.value)}
+      />
+
+      <button>Search</button>
+
+      <div>
+        <h1>Temp:{data.main.temp.toFixed()}°C</h1>
+      </div>
+
+      {/* {data.main.temp ? <h1>Temp:{data.main.temp}°C </h1> : null} */}
+    </header>
+  );
+};
 
 export default Header;
