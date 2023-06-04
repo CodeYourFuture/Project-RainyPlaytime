426b1aece6344925f9344fec235ec8a2881d8efb
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-06-04 21:01:53 +0100

    add alt to image

--- src/WeatherImages/WeatherImages.jsx
+++ src/WeatherImages/WeatherImages.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import WeatherImg from "../img/weather-icons/clear.svg"
-import Storm from "../img/weather-icons/storm.svg" 
+import Storm from "../img/weather-icons/storm.svg"
 import Drizzle from "../img/weather-icons/drizzle.svg"
 import Rain from "../img/weather-icons/rain.svg"
 import Snow from "../img/weather-icons/snow.svg"
@@ -13,15 +13,15 @@ const WeatherImages = ({infWeather}) => {
     <>
     {
         infWeather < 300 ? <img src={Storm} />
-        :  infWeather >= 300 && infWeather <= 499 ? <img 
-        src={Drizzle} /> : infWeather >= 500 && infWeather <= 599 ? <img 
-        src={Rain} /> : infWeather >= 600 && infWeather <= 699 ? <img 
-        src={Snow} /> : infWeather >= 700 && infWeather <= 799 ? <img 
-        src={Fog} /> : infWeather === 800 ? <img 
-        src={Clear} /> : infWeather === 801 ? <img 
-        src={PartlyCloudy} /> : infWeather >= 802 && infWeather <= 805 ? <img 
-        src={MostlyCloudy} /> :
-         <p>No Image Found</p> 
+        :  infWeather >= 300 && infWeather <= 499 ? <img
+        src={Drizzle}  alt='Drizzle'/> : infWeather >= 500 && infWeather <= 599 ? <img
+        src={Rain} alt='Rain' /> : infWeather >= 600 && infWeather <= 699 ? <img
+        src={Snow} alt='Snow' /> : infWeather >= 700 && infWeather <= 799 ? <img
+        src={Fog}  alt='Fog'/> : infWeather === 800 ? <img
+        src={Clear} alt='Clear' /> : infWeather === 801 ? <img
+        src={PartlyCloudy} alt='PartlyCloudy'/> : infWeather >= 802 && infWeather <= 805 ? <img
+        src={MostlyCloudy} alt='MostlyCloudy' /> :
+         <p>No Image Found</p>
     }
     </>
   )
