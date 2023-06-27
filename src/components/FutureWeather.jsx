4a6f0987046b1370158505353c821df33493d936
Zaw Khing <zawmyokhing@gmail.com>
2023-06-27 11:49:38 +0100

    Make image-icons bigger

--- src/components/FutureWeather.jsx
+++ src/components/FutureWeather.jsx
@@ -9,7 +9,9 @@ import WeatherIcon from "./Picture/WeatherIcon";
     <div className="container">
       <div className="items">
         <p>{formattedTime}</p>
-        <WeatherIcon weatherId={iconId} />
+        
+          <WeatherIcon weatherId={iconId} />
+        
         <p>{temp}°C</p>
       </div>
     </div>
@@ -17,20 +19,3 @@ import WeatherIcon from "./Picture/WeatherIcon";
 }
 export default FutureWeather;
 
-
-// function FutureWeather({ getNewWeather, weatherData }) {
-  
-//   return (
-//     <div>
-//       {weatherData?.list?.map((future) => (
-//         <div>
-//           <p>{future?.dt_txt?.split(" ")[1].slice(0, 5)}</p>
-//           <WeatherIcon>{future.weather[0].id}</WeatherIcon>
-//           <p>{future.main.temp.toFixed()}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default FutureWeather;
